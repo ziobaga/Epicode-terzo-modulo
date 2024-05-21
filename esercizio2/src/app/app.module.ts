df50a51695993cc94bb6c9059914048c7d3f01f6
ziobaga <adrianoleanza01@gmail.com>
2024-05-21 16:11:00 +0200

    ok

---
+++ esercizio2/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Component1Component } from './components/component1/component1.component';
+import { Component2Component } from './components/component2/component2.component';
+import { Component3Component } from './components/component3/component3.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Component1Component,
+    Component2Component,
+    Component3Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ esercizio2/src/app/components/component3/component3.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component3',
+  templateUrl: './component3.component.html',
+  styleUrl: './component3.component.scss'
+})
+export class Component3Component {
+
+}
